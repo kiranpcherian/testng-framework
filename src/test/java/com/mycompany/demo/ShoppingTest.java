8bd113f825d3194d5db1f28c4acc7df6e35a0e7b
Kiran Cherian <kiransara90@gmail.com>
2020-09-20 11:54:09 -0400

    Add shopping tests(product search, add to shopping cart).Closes #5

---
+++ src/test/java/com/mycompany/demo/ShoppingTest.java
@@ -0,0 +1,21 @@
+package com.mycompany.demo;
+
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class ShoppingTest {
+    @BeforeClass
+    public void beforeClass(){
+        System.out.println("Before ShoppingTests class is executed");
+    }
+
+    @Test
+    public void productSearch(){
+        System.out.println("Searching for a product");
+
+    }
+    @Test
+    public void addProductToShoppingCart(){
+        System.out.println("Adding a product to shopping cart");
+    }
+}
